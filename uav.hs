a19045906d9406f17797cbb851b15cebf9bff442
Gaurav Mahajan <gaurav.mahajan2@gmail.com>
2017-11-18 19:15:46 -0800

    Added comments.

--- uav.hs
+++ uav.hs
@@ -8,10 +8,16 @@ data Constraint = True |
                   And Constraint Constraint |
                   Or Constraint Constraint deriving (Show)
 
--- TODO: Update the checkConstraint implementation to call SMT solver 
+{- Algorithm: We ask dReach the following question: Starting from region specified by constraints,
+is it possible to end in a state not specified by the constraints? 
+If dReach says yes, then we have found a region which is safe/stable wrt battery and queue size.
+Otherwise, we add the counterexample and its implied space to the constraints. 
+-}
+-- TODO: Update the checkConstraint implementation to call dReach.
 checkConstraint :: Constraint -> (Double, Double)
 checkConstraint constraint = (100, 0)
 
+{- Adds the counterexample and its implied space to the constraints -}
 updateConstraint :: (Double, Double) -> Constraint -> Constraint
 updateConstraint (b,q) constraint = Or (And (GreaterThanEqual B b) (LessThanEqual Q q)) constraint
 
