a2c82fdae4a3937cc5985baef814f96f5a6c5237
Gaurav Mahajan <gaurav.mahajan2@gmail.com>
2017-11-18 16:45:16 -0800

    Added solve implementation.

--- uav.hs
+++ uav.hs
@@ -8,5 +8,15 @@ data Constraint = True |
                   And Constraint Constraint |
                   Or Constraint Constraint deriving (Show)
 
+-- TODO: Update the checkConstraint implementation to call SMT solver 
+checkConstraint :: Constraint -> (Double, Double)
+checkConstraint constraint = (100, 0)
+
 updateConstraint :: (Double, Double) -> Constraint -> Constraint
 updateConstraint (b,q) constraint = Or (And (GreaterThanEqual B b) (LessThanEqual Q q)) constraint
+
+solve :: Int -> Constraint -> Constraint
+solve n constraint 
+        | n > 0     = solve (n-1) constraint' 
+        | otherwise = constraint
+        where constraint' = updateConstraint (checkConstraint constraint) constraint
